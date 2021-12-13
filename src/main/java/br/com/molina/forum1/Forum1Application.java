ded8157b328bb0a4e06d06fe8b057d4a99ea839d
ElaineMolina <elainemolina10@gmail.com>
2021-12-13 08:40:25 -0300

    commit inicial

---
+++ src/main/java/br/com/molina/forum1/Forum1Application.java
@@ -0,0 +1,15 @@
+package br.com.molina.forum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Forum1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Forum1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/molina/forum1/Forum1ApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.molina.forum1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+    @SpringBootTest
+    class Forum1ApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+
+
