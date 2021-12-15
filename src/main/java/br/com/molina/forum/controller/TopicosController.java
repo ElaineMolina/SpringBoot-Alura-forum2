f51ecb7ca6a4ebdae0772f64f1754efda0968929
ElaineMolina <elainemolina10@gmail.com>
2021-12-15 17:16:31 -0300

    Utilizando Repository. Configurando "devtools" no intellij

--- src/main/java/br/com/molina/forum/controller/TopicosController.java
+++ src/main/java/br/com/molina/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.molina.forum.controller;
 import br.com.molina.forum.controller.dto.TopicoDto;
 import br.com.molina.forum.modelo.Curso;
 import br.com.molina.forum.modelo.Topico;
+import br.com.molina.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,12 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Sprinig", "Programação"));
-
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
 }
--- src/main/java/br/com/molina/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/molina/forum/repository/TopicoRepository.java
@@ -1,4 +1,9 @@
 package br.com.molina.forum.repository;
 
-public interface TopicoRepository {
+import br.com.molina.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+
 }
