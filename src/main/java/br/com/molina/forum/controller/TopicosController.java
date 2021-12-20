261990cfbdb69cce82e7dbf495bcec75228128c9
ElaineMolina <elainemolina10@gmail.com>
2021-12-20 11:57:24 -0300

    Ordenação

--- src/main/java/br/com/molina/forum/controller/TopicosController.java
+++ src/main/java/br/com/molina/forum/controller/TopicosController.java
@@ -5,10 +5,12 @@ import br.com.molina.forum.controller.form.TopicoForm;
 import br.com.molina.forum.modelo.Topico;
 import br.com.molina.forum.repository.CursoRepository;
 import br.com.molina.forum.repository.TopicoRepository;
+import org.hibernate.internal.CoordinatingEntityNameResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,8 +30,11 @@ public class TopicosController {
 
     @GetMapping
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina, @RequestParam int qtd){
-        Pageable paginacao = PageRequest.of(pagina, qtd);
+                                 @RequestParam int pagina, @RequestParam int qtd,
+                                 @RequestParam String ordenacao){
+
+        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
+
 
         Page<Topico> topicos;
         if(nomeCurso == null){
